4b856f65d8e128ca68f55aa508975c1d43121de1
Camilo Laiton <camilo.laiton@alleninstitute.org>
2022-08-01 14:10:09 -0700

    Creating execution pipeline with a dictionary as input

--- code/src/terastitcher.py
+++ code/src/terastitcher.py
@@ -57,6 +57,7 @@ class TeraStitcher():
             parallel:bool=True,
             parastitcher_path:Optional[str]=None,
             computation:Optional[str]='cpu',
+            verbose:Optional[bool]=False
         ) -> None:
         
         """
@@ -74,6 +75,7 @@ class TeraStitcher():
         self.__computation = computation
         self.__platform = platform.system()
         self.__parastitcher_path = parastitcher_path
+        self.__verbose = verbose
         
         if computation not in ['cpu', 'gpu']:
             print("Setting computation to gpu")
@@ -124,11 +126,10 @@ class TeraStitcher():
         mpi_command = 'mpiexec -n' if self.__platform == 'Windows' else 'mpirun -np'
         
         additional_params = ''
-        
         if len(params['additional_params']):
             additional_params = helper_additional_params_command(params['additional_params'])
             
-        cmd = f"{mpi_command} {params['number_processes']} --hostfile {params['hostfile_path']} {additional_params}"
+        cmd = f"{mpi_command} {params['number_processes']} {additional_params}"
         cmd += f"python {self.__parastitcher_path}"
         return cmd
     
@@ -148,7 +149,7 @@ class TeraStitcher():
         
         cmd = f"terastitcher --import {volume_input} {output_folder} {parameters} {additional_params}"
         
-        utils.save_dict_as_json(f"{self.__output_folder}/metadata/import_params.json", params, True)
+        utils.save_dict_as_json(f"{self.__output_folder}/metadata/import_params.json", params, self.__verbose)
         
         return cmd
     
@@ -158,7 +159,7 @@ class TeraStitcher():
         
         # TODO Check the best number of processes using formula
         input_xml = f"--projin={self.__output_folder}/xmls/xml_import.xml"
-        output_xml = f"--projout={self.__output_folder}/xmls/xml_displcomp_par2.xml"
+        output_xml = f"--projout={self.__output_folder}/xmls/xml_displcomp.xml"
         parallel_command = ''
         
         if self.__parallel:
@@ -178,18 +179,60 @@ class TeraStitcher():
         
         parameters = helper_build_param_value_command(params)
         
-        cmd = f"{parallel_command} --displcompute {input_xml} {output_xml} {parameters}"
-        utils.save_dict_as_json(f"{self.__output_folder}/metadata/align_params.json", params, True)
+        cmd = f"{parallel_command} --displcompute {input_xml} {output_xml} {parameters} > {self.__output_folder}/xmls/step2par.txt"
+        utils.save_dict_as_json(f"{self.__output_folder}/metadata/align_params.json", params, self.__verbose)
         
         return cmd
     
-    def __input_output_step_cmd(self, step_name:str, input_xml:str, output_xml:str) -> str:
+    def __input_output_step_cmd(self, 
+            step_name:str, 
+            input_xml:str, 
+            output_xml:str, 
+            params:Optional[dict]=None,
+        ) -> str:
+        
         input_xml = f"--projin={self.__output_folder}/xmls/{input_xml}"
         output_xml = f"--projout={self.__output_folder}/xmls/{output_xml}"
+        parameters = ''
         
-        cmd = f"terastitcher --{step_name} {input_xml} {output_xml}"
+        if params:
+            parameters = helper_build_param_value_command(params)
+        
+        cmd = f"terastitcher --{step_name} {input_xml} {output_xml} {parameters}"
+        utils.save_dict_as_json(f"{self.__output_folder}/metadata/{step_name}_params.json", params, self.__verbose)
         return cmd
+    
+    def __merge_step_cmd(self, params:dict):
+        """
+        """
         
+        # TODO Check the best number of processes using formula
+        input_xml = f"--projin={self.__output_folder}/xmls/xml_merging.xml"
+        output_path = f"--volout={self.__output_folder}"
+        parallel_command = ''
+        
+        if self.__parallel:
+            
+            if self.__computation == 'cpu':
+                # mpirun for linux and mac OS
+                parallel_command = self.__build_parallel_command(params['cpu_params'])
+                
+            else:
+                # gpu computation
+                # TODO set gpu flag
+                pass
+        
+        else:
+            # Sequential execution
+            parallel_command = f"terastitcher"
+        
+        parameters = helper_build_param_value_command(params)
+        
+        cmd = f"{parallel_command} --merge {input_xml} {output_path} {parameters} > {self.__output_folder}/xmls/step6par.txt"
+        utils.save_dict_as_json(f"{self.__output_folder}/metadata/merge_params.json", params, self.__verbose)
+        
+        return cmd
+    
     def execute_pipeline(self, config:dict) -> None:
         
         """
@@ -197,26 +240,56 @@ class TeraStitcher():
         """
         
         # Step 1
-        print(self.__import_step_cmd(config['import']))
+        print("\n- Import step...")
+        for out in utils.execute_command(
+            self.__import_step_cmd(config['import']), self.__verbose
+        ):
+            print(out)
         
         # Step 2
-        print(self.__align_step_cmd(config['align']))
+        print("- Align step...")
+        for out in utils.execute_command(
+            self.__align_step_cmd(config['align']), self.__verbose
+        ):
+            print(out)
         
         # Step 3
-        print(self.__input_output_step_cmd('placetiles', 'xml_displthres.xml', 'xml_merging.xml'))
-        
+        print("- Projection step...")
+        for out in utils.execute_command(
+            self.__input_output_step_cmd(
+                'displproj', 'xml_displcomp.xml', 'xml_displproj.xml'
+            ), self.__verbose
+        ):
+            print(out)
+          
         # Step 4
-        print(self.__input_output_step_cmd('displthres', 'xml_displproj.xml', 'xml_displthres.xml'))
+        print("- Threshold step...")
+        for out in utils.execute_command(
+            self.__input_output_step_cmd(
+                'displthres', 'xml_displproj.xml', 'xml_displthres.xml', config['threshold']
+            ), self.__verbose
+        ):
+            print(out)
         
         # Step 5
-        print(self.__input_output_step_cmd('displproj', 'xml_displcomp_par2.xml', 'xml_displproj.xml'))
+        print("- Placing tiles step...")
+        for out in utils.execute_command(
+            self.__input_output_step_cmd(
+                'placetiles', 'xml_displthres.xml', 'xml_merging.xml'
+            ), self.__verbose
+        ):
+            print(out)
         
         # Step 6
-        
-    
+        print("- Merging step...")
+        for out in utils.execute_command(
+            self.__merge_step_cmd(config["merge"]), self.__verbose
+        ):
+            print(out)
+
 def main():
     input_data = "C:/Users/camilo.laiton/Documents/Project1/Terastitcher/TestData/mouse.cerebellum.300511.sub3/tomo300511_subv3"
-    output_folder = "C:/Users/camilo.laiton/Documents/Project1/Terastitcher/TestData/output_python"
+    output_folder = "C:/Users/camilo.laiton/Documents/Project1/Terastitcher/TestData/output_python_3d"
     
     # TODO if we pass another path that exists instead of parastitcher's path, it builds the command
     parastitcher_path = 'C:/Users/camilo.laiton/Documents/Project1/Terastitcher/TeraStitcher-portable-1.11.10-win64/pyscripts/Parastitcher.py'
@@ -226,26 +299,40 @@ def main():
         output_folder=output_folder,
         parallel=True,
         computation='cpu',
-        parastitcher_path=parastitcher_path
+        parastitcher_path=parastitcher_path,
+        verbose=False
     )
 
     config = {
         "import" : {
-            'ref1':'H',
-            'ref2':'V',
-            'ref3':'D',
-            'vxl1':'1.800',
-            'vxl2':'1.800',
-            'vxl3':'2',
+            'ref1':'1',#'H',
+            'ref2':'-2',#'V',
+            'ref3':'3',#'D',
+            'vxl1':'0.8',#'1.800',
+            'vxl2':'0.8',#'1.800',
+            'vxl3':'1',#'2',
             'additional_params':['sparse_data', 'libtiff_uncompress'] # 'rescan'
         },
         "align" : {
             'cpu_params' : {
-                'number_processes': 6,
-                'hostfile_path': 'path/to/hostfile',
-                'additional_params' : ['use-hwthread-cpus', 'allow-run-as-root']
+                'number_processes': 6, # np for linux or mac
+                #'hostfile': 'C:/Users/camilo.laiton/Documents/Project1/Terastitcher/TestData',
+                'additional_params' : [] # ['use-hwthread-cpus', 'allow-run-as-root']
             },
             'subvoldim': 100,
+        },
+        "threshold" : {
+            "threshold" : 0.7
+        },
+        "merge" : {
+            'cpu_params' : {
+                'number_processes': 6, # np for linux or mac
+                #'hostfile': 'C:/Users/camilo.laiton/Documents/Project1/Terastitcher/TestData',
+                'additional_params' : [] # ['use-hwthread-cpus', 'allow-run-as-root']
+            },
+            'volout_plugin' : '"TiledXY|2Dseries"', # This parameter need to be provided with "" TiledXY|3Dseries
+            'slicewidth' : 20000,
+            'sliceheight' : 20000,
         }
     }
     
--- code/src/utils/utils.py
+++ code/src/utils/utils.py
@@ -27,32 +27,40 @@ def create_folder(dest_dir:str) -> None:
         except OSError as e:
             if (e.errno != os.errno.EEXIST):
                 raise
-            
-def execute_command(command:str) -> None:
-        """
-        Execute a shell command.
-        
-        Parameters
-        ------------------------
-        
-            - command (str): Command that we want to execute.
-        
-        Returns
-        ------------------------
-            - None
-        """
-        popen = subprocess.Popen(command, stdout=subprocess.PIPE, universal_newlines=True, shell=True)
-        for stdout_line in iter(popen.stdout.readline, ""):
-            yield stdout_line
-        popen.stdout.close()
-        return_code = popen.wait()
-        if return_code:
-            raise subprocess.CalledProcessError(return_code, command)
+
+def execute_command(command:str, print_command:bool=False) -> None:
+    """
+    Execute a shell command.
+    
+    Parameters
+    ------------------------
+    
+        - command (str): Command that we want to execute.
+        - print_command (bool): Bool that dictates if we print the command in the console.
+    
+    Returns
+    ------------------------
+        - None
+    """
+    
+    if print_command:
+        print(command)
+    
+    popen = subprocess.Popen(command, stdout=subprocess.PIPE, universal_newlines=True, shell=True)
+    for stdout_line in iter(popen.stdout.readline, ""):
+        yield stdout_line
+    popen.stdout.close()
+    return_code = popen.wait()
+    if return_code:
+        raise subprocess.CalledProcessError(return_code, command)
         
 def save_dict_as_json(filename:str, dictionary:dict, verbose:False) -> None:
     """
     
     """
+    if dictionary == None:
+        dictionary = {}
+    
     with open(filename, "w") as json_file:
         json.dump(dictionary, json_file, indent=4)
     
