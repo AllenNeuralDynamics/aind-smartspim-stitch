b3500b3da569ba3228b6e7de60599d69bacdf52f
Camilo Laiton <36769694+camilolaiton@users.noreply.github.com>
2022-10-25 09:12:53 -0700

    fixing timestamp

--- code/src/utils/utils.py
+++ code/src/utils/utils.py
@@ -437,7 +437,7 @@ def check_type_helper(value:Any, val_type:type) -> bool:
     
     return True
 
-def generate_timestamp(time_format:str='%Y-%m-%d-%H-%M-%S') -> str:
+def generate_timestamp(time_format:str='%Y-%m-%d_%H-%M-%S') -> str:
     """
     Generates a timestamp in string format.
     
