3061c28f7c4957a39dfa0c23e9029521e11aa76f
Camilo Laiton <36769694+camilolaiton@users.noreply.github.com>
2023-03-06 10:05:02 -0800

    updating main file to avoid writing to the external bucket

--- code/main.py
+++ code/main.py
@@ -5,7 +5,6 @@ import logging
 import sys
 
 from aind_smartspim_stitch import terastitcher
-from aind_smartspim_stitch.utils import utils
 
 
 def main() -> None:
@@ -29,17 +28,6 @@ def main() -> None:
     logger.setLevel(logging.INFO)
 
     logger.info(f"Bucket path: {bucket_path} - Output path: {output_folder}")
-    co_folder = output_folder.split('/')[1]
-    
-    # Copying output to bucket
-    dataset_name = output_folder.replace(f"/{co_folder}/", "")
-    s3_path = f"s3://{bucket_path}/{dataset_name}"
-    for out in utils.execute_command_helper(f"aws s3 mv --recursive {output_folder} {s3_path}"):
-        logger.info(out)
-
-    utils.save_string_to_txt(
-        f"Stitched dataset saved in: {s3_path}", "/root/capsule/results/output_stitching.txt",
-    )
 
 
 if __name__ == "__main__":
