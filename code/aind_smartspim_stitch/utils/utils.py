623d37f8159bd031285845a4edd581a1c0a0cd48
Camilo Laiton <36769694+camilolaiton@users.noreply.github.com>
2024-11-09 15:19:47 -0500

    updating CO utilities

--- code/aind_smartspim_stitch/utils/utils.py
+++ code/aind_smartspim_stitch/utils/utils.py
@@ -747,12 +747,17 @@ def print_system_information(logger: logging.Logger):
     logger: logging.Logger
         Logger object
     """
-    co_memory = int(os.environ.get("CO_MEMORY"))
+    co_memory = os.environ.get("CO_MEMORY")
+    co_memory = int(co_memory) if co_memory else None
+
     # System info
     sep = "=" * 40
     logger.info(f"{sep} Code Ocean Information {sep}")
     logger.info(f"Code Ocean assigned cores: {get_code_ocean_cpu_limit()}")
-    logger.info(f"Code Ocean assigned memory: {get_size(co_memory)}")
+
+    if co_memory:
+        logger.info(f"Code Ocean assigned memory: {get_size(co_memory)}")
+
     logger.info(f"Computation ID: {os.environ.get('CO_COMPUTATION_ID')}")
     logger.info(f"Capsule ID: {os.environ.get('CO_CAPSULE_ID')}")
     logger.info(f"Is pipeline execution?: {bool(os.environ.get('AWS_BATCH_JOB_ID'))}")
