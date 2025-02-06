a91d6003eaf73301c02fe63ca69e6853f163660c
camilolaiton <camilo.laiton@alleninstitute.org>
2025-02-06 05:13:16 +0000

    Updating init file

--- code/aind_smartspim_stitch/__init__.py
+++ code/aind_smartspim_stitch/__init__.py
@@ -3,3 +3,21 @@ Init file
 """
 
 __version__ = "1.2.5"
+__authors__ = ["Camilo Laiton"]
+__author_emails__ = [
+    "camilo.laiton@alleninstitute.org",
+]
+__license__ = "MIT"
+__description__ = (
+    "Image preprocessing for flatfield correction and "
+    "image destriping"
+)
+__url__ = "https://github.com/AllenNeuralDynamics/aind-smartspim-destripe"
+
+__maintainers__ = ["Camilo Laiton"]
+__maintainer_emails__ = [
+    "camilo.laiton@alleninstitute.org",
+]
+__title__ = "aind-smartspim-destripe"
+__status__ = "Production"  # 'Development' 'Production', 'Beta'
+__pipeline_version__ = "3.0.0"
